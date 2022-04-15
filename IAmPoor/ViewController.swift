f78a79b1db765d99e7ede5683a2e0142093e1dda
Denis <Denis@Mac-mini.local>
2022-04-14 21:21:10 -0300

    Adding initial source code.

---
+++ IAmPoor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IAmPoor
+//
+//  Created by Denis on 22/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
